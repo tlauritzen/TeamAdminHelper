8c3c92035bb12a5ecd457653f378b86dd8e2961b
Torben Lauritzen <tl@magus.dk>
2012-02-21 19:35:17 +0100

    Cleanup

--- php/js/init.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    alert("As you can see, the link no longer took you to jquery.com");
-});
-
--- php/view/login.php
+++
@@ -1,9 +0,0 @@
-<h1><?=$title?></h1>
-
-<form style="padding-top: 100px" method="get" action="controller/dologin.php">
-  <div class="input">Username<input style="left: 50%" id="username" name="username" type="text" class="field" /></div>
-  <div class="input">Password<input id="password" name="password" type="password" class="field" /></div>
-  <div class="input" style="width: 100%"><input type="submit" class="text" value="login" /></div>
-</form>
-
-
